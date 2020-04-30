8540dbd81452538ee7d8130d69f973af9e9c5238
Philipp Schubert <philipp@it-schubert.com>
2020-04-30 16:01:03 +0200

    minor

--- utils/InstallAptDependencies.sh
+++ utils/InstallAptDependencies.sh
@@ -6,4 +6,4 @@ sudo apt-get install git make cmake -y
 #echo "-------------------------------------------"
 #git --version
 #echo "-------------------------------------------"
-sudo apt-get install zlib1g-dev sqlite3 libsqlite3-dev python3 doxygen graphviz python python-dev python3-pip python-pip libxml2 libxml2-dev libncurses5-dev libncursesw5-dev swig build-essential g++ cmake libedit-dev python-sphinx libomp-dev libcurl4-openssl-dev -y
+sudo apt-get install zlib1g-dev sqlite3 libsqlite3-dev python3 doxygen graphviz python3 python3-dev python3-pip libxml2 libxml2-dev libncurses5-dev libncursesw5-dev swig build-essential g++ cmake libedit-dev python3-sphinx libomp-dev libcurl4-openssl-dev ninja-build -y
